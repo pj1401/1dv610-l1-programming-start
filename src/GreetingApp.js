0ae83391b3e4e37845001b44ab3ceb73ca352507
Patricia Johansson <142814895+pj1401@users.noreply.github.com>
2024-09-09 15:07:08 +0200

    Feature/name input (#2)
    * Add run methods to greeting class
    
    * Get a greeting after input

--- src/GreetingApp.js
+++ src/GreetingApp.js
@@ -10,6 +10,8 @@
 export class GreetingApp {
   #name
 
+  #greeting
+
   /**
    * Initialises a new instance.
    *
@@ -17,5 +19,32 @@ export class GreetingApp {
    */
   constructor (name) {
     this.#name = name
+  }
+
+  /**
+   * Returns a string representing the object.
+   *
+   * @returns {string} A greeting.
+   */
+  toString () {
+    return this.#greeting.join('')
+  }
+
+  /**
+   * Run the greeting application.
+   */
+  async run () {
+    this.#greeting = []
+    this.#greeting.push('Hello\n')
+
+    const chars = this.#name.split('')
+
+    for (let i = 0; i < chars.length; i++) {
+      for (let j = 0; j < chars.length; j++) {
+        this.#greeting.push(chars[j])
+      }
+      chars.pop()
+      this.#greeting.push('\n')
+    }
   }
 }
