f33ecc1d9bd6e474b1f7b98d2601772f16eee017
Patricia Johansson <142814895+pj1401@users.noreply.github.com>
2024-09-09 17:12:12 +0200

    Feature/greeting (#3)
    * Return the final greeting

--- src/GreetingApp.js
+++ src/GreetingApp.js
@@ -35,16 +35,32 @@ export class GreetingApp {
    */
   async run () {
     this.#greeting = []
-    this.#greeting.push('Hello\n')
 
-    const chars = this.#name.split('')
+    const hello = 'Hello '
 
-    for (let i = 0; i < chars.length; i++) {
+    // Split the greeting into chars.
+    const chars = hello.split('').concat([...this.#name.split(''), '!'])
+
+    const usedChars = []
+
+    for (let i = 0, length = chars.length; i < length; i++) {
       for (let j = 0; j < chars.length; j++) {
         this.#greeting.push(chars[j])
       }
-      chars.pop()
+      usedChars.push(chars.pop())
       this.#greeting.push('\n')
+    }
+
+    this.#greeting.push('\n')
+
+    // Get the greeting in reverse.
+    for (let i = 0, length = usedChars.length; i < length; i++) {
+      for (let j = 0; j <= i; j++) {
+        this.#greeting.push(usedChars[j])
+      }
+      if (i !== length - 1) {
+        this.#greeting.push('\n')
+      }
     }
   }
 }
